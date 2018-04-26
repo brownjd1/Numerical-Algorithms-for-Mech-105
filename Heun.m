50bf61702296492e901179b6dba982bb792b5053
jacobbrown <jacobbrown@github.com>
2018-04-25 20:19:11 -0600

    Init Heun algorithm

---
+++ Heun.m
@@ -0,0 +1,7 @@
+function [t,y] = Heun(dydt,tspan,y0,h,es,maxit)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
