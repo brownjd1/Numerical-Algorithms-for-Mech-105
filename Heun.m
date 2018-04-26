7adb8b09d076f886aff7961aa6f82960bb4c2981
jacobbrown <jacobbrown@github.com>
2018-04-25 21:07:43 -0600

    Edited function

--- Heun.m
+++ Heun.m
@@ -2,10 +2,10 @@ function [t,y] = Heun(dydt,tspan,y0,h,es,maxit)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 err = 100;% Initialize error
-while err < es
-    err = %(abs((guess2 - guess1))/guess1)*100;
+s = length(tspan);% Find size of domain
+for i = tspan(1) : tspan(s)% Initialize for loop to run for the domain
+    while err < es% embeded while loop runs until approx error condition is met
+        slopel = dydt(
+        err = %(abs((guess2 - guess1))/guess1)*100;
+    end
 end
-end
-
-zpwoieufjpwifouenpzoif
-Sammy is great]=
