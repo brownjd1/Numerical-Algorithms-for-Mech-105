fb526e72411e347328c40d7058a3e1fb38f135c5
jacobbrown <jacobbrown@github.com>
2018-04-26 23:52:04 -0600

    add counters

--- Heun.m
+++ Heun.m
@@ -4,13 +4,16 @@ function [t,y] = Heun(dydt,tspan,y0,h,es,maxit)
 err = 100;% Initialize error
 s = length(tspan);% Find size of domain
 for i = tspan(1) : tspan(s)% Initialize for loop to run for the domain
-    while err < es% embeded while loop runs until approx error condition is met
-        sloperight = %evaluate dydt at y0 and i to find predictor y value at i + 1
+    iter = 0;% Initialize iteration counter
+    while err < es && iter < maxit% embeded while loop runs until approx error condition is met
+        sloperight =  %evaluate dydt at y0 and i to find predictor y value at i + 1
         %evaluate dydt at predictor y at i + 1
         slopeleft = %find the average value between y(i) and y(i+1) (y0 +
         %(h/2)*(slopeleft + sloperight)
         y(i) = %use corrector value to get a better estimate at y(i+1)
         err = %(abs((guess2 - guess1))/guess1)*100;
+        iter = iter + 1;
+        
     end
 end
 
