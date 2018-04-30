0fc23c5476ee5c5e402b7962569b6180fce163e0
Jacob Brown <jacobbrown@Jacobs-MacBook-Pro.local>
2018-04-30 00:10:08 -0600

    tried some new things

--- Heun.m
+++ Heun.m
@@ -8,11 +8,11 @@ for i = tspan(1) : tspan(s) : h% Initialize for loop to run for the domain
     guess2 = y0;% Create variable to mark the guess
     while err > es || iter < maxit% embeded while loop runs until approx error condition is met
         slopeleft = dydt(i,y0);%evaluate dydt at y0 and i to find predictor slope
-        guess1 = guess2 + h * slopeleft;%Use Euler's method to find y at i + 1
-        sloperight = dydt(i+1,guess1);
-        slopecorrect = (y0 + (h/2) * (slopeleft + sloperight)); %find the average value between y(i) and y(i+1) (y0 +
+        guess1 = y0 + (h * slopeleft);%Use Euler's method to find y at i + 1
+        sloperight = dydt(i+h,guess1);
+        corrector = (y0 + ((h/2) * (slopeleft + sloperight))); %find the average value between y(i) and y(i+1) (y0 +
         %(h/2)*(slopeleft + sloperight)
-        guess2 =  guess2 + h * slopecorrect;%use corrector value to get a better estimate at y(i+1)
+        guess2 =  guess2 + (h * slopecorrect);%use corrector value to get a better estimate at y(i+1)
         err = ((abs(guess2 - guess1))/guess1)*100;
         iter = iter + 1;
     end
